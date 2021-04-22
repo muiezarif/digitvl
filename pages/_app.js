87adaf7e2e964bdb626b733de168e683a910f3d2
muiezarif <muiezarif@gmail.com>
2021-04-22 16:53:36 -0700

    store file updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
 import {Provider} from "react-redux";
+import { useMemo } from 'react'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunkMiddleware from 'redux-thunk'
+import reducers from '../reducers'
 import { ThemeProvider } from '@material-ui/core/styles';
-import { useStore } from "./store";
+// import { useStore } from "./store";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme.js';
 import '../styles/globals.css'
@@ -21,6 +26,7 @@ import ReactNotification from "react-notifications-component";
 const STRIPE_PUBLISHABLE_KEY = "pk_live_vwxZQV1ljD5wUdGz3WQRbPr900EibgZ1HH"
 // const STRIPE_PUBLISHABLE_KEY = "pk_test_LPbi74JE59NNYpohjEuJHf15004en8ReKA"
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY)
+let store
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
   React.useEffect(() => {
@@ -44,5 +50,38 @@ function MyApp({ Component, pageProps }) {
   </React.Fragment>
   </Provider></Elements>);
 }
+function initStore(initialState) {
+  return createStore(
+      reducers,
+      initialState,
+      composeWithDevTools(applyMiddleware(thunkMiddleware))
+  )
+}
 
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+export function useStore(initialState) {
+  const store = useMemo(() => initializeStore(initialState), [initialState])
+  return store
+}
 export default MyApp
