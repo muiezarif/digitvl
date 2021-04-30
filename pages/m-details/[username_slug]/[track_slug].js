aa77502f76e47b2acbfb49508535e8cc3abf3f83
muiezarif <muiezarif@gmail.com>
2021-04-29 17:01:31 -0700

    seo fixed on music detail page

--- pages/m-details/[username_slug]/[track_slug].js
+++ pages/m-details/[username_slug]/[track_slug].js
@@ -2,14 +2,28 @@ import React from "react";
 import {useRouter} from "next/router";
 import MusicDetail from "../../../components/MusicDetail";
 import {NextSeo} from "next-seo";
+import useSWR from "swr"
 
 
-export default function mDetails({seoprops}){
+async function fetcherFunction(url) {
+    const res = await fetch(url)
+    return res.json()
+}
+
+export default function mDetails(props){
     const router = useRouter();
-    console.log(seoprops)
+    const url = "https://novamdigital.com/api/v1/beats/"+router.query.username_slug+"/"+router.query.track_slug
+    const {data, error} = useSWR(url,fetcherFunction,{initialData:props})
+    if (error){
+        return <div>Failed to Load</div>
+    }
+    if (!data){
+        return <div>Loading</div>
+    }
+    console.log(data)
     return (
         <div>
-            <MusicDetail dataparams={router.query} seoparams={seoprops} />
+            <MusicDetail dataparams={router.query} seoparams={data} />
         </div>
     );
 };
@@ -26,5 +40,5 @@ export async function getServerSideProps(context){
     const res = await fetch('https://novamdigital.com/api/v1/beats/'+context.params.username_slug+'/'+context.params.track_slug)
     // const error_code = res.statusCode > 200 ? res.statusCode : false;
     const seodata = await res.json();
-    return {props: {seoprops:seodata}}
+    return {props: seodata}
 }
