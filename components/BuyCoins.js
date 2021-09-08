90fabd90c94cfd4b5482d0f689ad49e03fa4b1c7
muiezarif <muiezarif@gmail.com>
2021-09-09 01:07:06 +0500

    buys coins added loader

--- components/BuyCoins.js
+++ components/BuyCoins.js
@@ -1,12 +1,15 @@
-import React, {Component} from 'react';
+import React, {Component,useState} from 'react';
 import Navbar from "./Navbar";
 import {CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
 import axios from "axios";
-
+import {
+    Wave
+} from 'better-react-spinkit'
 let userSession
 const BuyCoins = () => {
     const stripe = useStripe()
     const elements = useElements()
+    const [loader,showLoader] = useState(false)
     const createPayment = async (userSession,priceId) => {
         console.log(userSession)
         const API = axios.create({
@@ -25,6 +28,7 @@ const BuyCoins = () => {
             headers: {'Authorization': `jwt ${userSession}`}
         })
         console.log(response)
+        showLoader(false)
         const result = stripe.redirectToCheckout({
             sessionId: response.data.id
         })
@@ -41,7 +45,9 @@ const BuyCoins = () => {
         userSession = window.localStorage.getItem("userToken")
         // var priceId = "price_1JAXgAI4e8u2GP8q67oEupda"  //dev
         var priceId = "price_1JSfobI4e8u2GP8qXxeOpdTR"  //live
+        showLoader(true)
         createPayment(userSession,priceId)
+
     }
     const productPayment250 = async (e) => {
         e.preventDefault()
@@ -50,7 +56,8 @@ const BuyCoins = () => {
         }
         userSession = window.localStorage.getItem("userToken")
         // var priceId = "price_1JQrxSI4e8u2GP8qns1sBlzW" //dev
-        var priceId = "price_1JSfonI4e8u2GP8qU3qOzt6I"    //live
+        var priceId = "price_1JSfonI4e8u2GP8qU3qOzt6I"
+        showLoader(true)//live
         createPayment(userSession,priceId)
     }
     const productPayment500 = async (e) => {
@@ -60,7 +67,8 @@ const BuyCoins = () => {
         }
         userSession = window.localStorage.getItem("userToken")
         // var priceId = "price_1JQruiI4e8u2GP8qGa17DH4q" //dev
-        var priceId = "price_1JSfofI4e8u2GP8q19FDEwCK"    //live
+        var priceId = "price_1JSfofI4e8u2GP8q19FDEwCK"
+        showLoader(true)//live
         createPayment(userSession,priceId)
     }
     return (
@@ -69,6 +77,9 @@ const BuyCoins = () => {
             <div className="custom-trending-heading">
                 Buy DIGITVL coins
             </div>
+            {loader ? <div className="row custom-row-margin justify-content-center text-center">
+                <Wave size={50}  color="steelblue" className="" name="line-scale"/>
+            </div> : null}
             <div className="container mt-5 mx-auto row">
                 <div className="container-fluid custom-blogs-item-bg">
                     <div className="row custom-row-margin col-md-12 col-sm-6 col-xs-6 pt-2 pb-2 custom-trending-item-bg">
