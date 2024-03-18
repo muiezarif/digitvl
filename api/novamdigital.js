a14e425bb96a58a3d5c840ce0d3bb1330fb16370
muiezarif <36100308+muiezarif@users.noreply.github.com>
2024-03-19 01:36:37 +0500

    url changed

--- api/novamdigital.js
+++ api/novamdigital.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default axios.create({
     // baseURL:"http://157.230.177.2:8001/api/v1"
-    baseURL:"https://novamdigital.com/api/v1"
-    // baseURL:"https://digitvl.live/api/v1"
+    // baseURL:"https://novamdigital.com/api/v1"
+    baseURL:"https://digitvl.xyz/api/v1"
     // baseURL:"https://96db-119-63-138-150.ngrok.io/api/v1"
 });
